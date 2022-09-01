5ba0ea4580ef17ef3deca9f09f0ed3b283595454
Daniel Aklog <aklogdaniel@gmail.com>
2022-09-01 10:09:04 +0300

    created tabs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Tabs } from "@mantine/core";
 
 function App() {
+  const [leagueIn, setLeagueIn] = useState(0);
+  const league_in_titles = ["In the News", "Press"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="page-header">
+        <span className="gray-text">Enterprise League </span>
+        <span className="blue-text">{league_in_titles[leagueIn]}</span>
+      </div>
+      <Tabs color="#228cd5" value={leagueIn} onTabChange={setLeagueIn}>
+        <Tabs.List>
+          <Tabs.Tab value="0">{league_in_titles[0]}</Tabs.Tab>
+          <Tabs.Tab value="1">{league_in_titles[1]}</Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value="0">First panel</Tabs.Panel>
+        <Tabs.Panel value="1">Second panel</Tabs.Panel>
+      </Tabs>
     </div>
   );
 }
